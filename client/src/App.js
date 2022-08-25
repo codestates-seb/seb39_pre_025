e3a3719e1bd569f5327e9c04d4f00ec0a0cb8737
songseul <song.codestates@gmail.com>
2022-08-25 11:13:33 +0900

    button 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,60 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return <div>App</div>;
+  const [data, setData] = useState(null);
+
+  const onClick = async e => {
+    e.preventDefault();
+    await axios
+      .get('http://localhost:8080/api/test')
+      .then(res => {
+        setData(res.data);
+      })
+
+      .catch(err => console.log(`${err}`));
+  };
+
+  return (
+    <div
+      style={{
+        marginTop: '30%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <button
+        onClick={onClick}
+        style={{
+          width: 200,
+          height: 80,
+          backgroundColor: '#078AFF',
+          borderRadius: '10px',
+          color: '#fff',
+          border: 'none',
+          cursor: 'pointer',
+          textAlign: 'center',
+        }}
+      >
+        버튼입니다
+      </button>
+
+      <div
+        style={{
+          margin: 10,
+          border: '1px solid #000',
+          borderRadius: '10px',
+          width: 400,
+          height: 200,
+        }}
+      >
+        {data ? data : null}
+      </div>
+    </div>
+  );
 };
 
 export default App;
