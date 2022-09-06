7e094763988c972b1acb1e6564a268b13052f987
songseul <song.codestates@gmail.com>
2022-09-06 13:05:25 +0900

    fix: CSS 최종

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,68 +6,70 @@ import { faStackOverflow } from '@fortawesome/fontawesome-free-brands';
 function Footer() {
   return (
     <FooterLayout>
-      <LogoBox>
-        <FontAwesomeIcon icon={faStackOverflow} size="2x" />
-      </LogoBox>
-      <Col>
-        <ul>
-          <div className="title">STACK OVERFLOW</div>
-          <li>Questions</li>
-          <li>Help</li>
-        </ul>
-      </Col>
-      <Col>
-        <ul>
-          <div className="title">PRODUCTS</div>
-          <li>Teams</li>
-          <li>Advertising</li>
-          <li>Collectives</li>
-          <li>Talent</li>
-        </ul>
-      </Col>
-      <Col>
-        <ul>
-          <div className="title">COMPANY</div>
-          <li>About</li>
-          <li>Press</li>
-          <li>Work Here</li>
-          <li>Legal</li>
-          <li>Privacy Policy</li>
-          <li>Terms of Service</li>
-          <li>Contact Us</li>
-          <li>Cookie Settings</li>
-          <li>Cookie Policy</li>
-        </ul>
-      </Col>
-      <Col>
-        <ul>
-          <div className="title">STACK EXCHANGE NETWORK</div>
-          <li>Technology</li>
-          <li>Culture & recreation</li>
-          <li>Life & arts</li>
-          <li>Science</li>
-          <li>Professional</li>
-          <li>Business</li>
-          <li className="hr">안보이지</li>
-          <li>API</li>
-          <li>Data</li>
-        </ul>
-      </Col>
-      <SocialBox>
-        <ul>
-          <li>Blog</li>
-          <li>Facebook</li>
-          <li>Twitter</li>
-          <li>Linkedin</li>
-          <li>Instagram</li>
-        </ul>
-        <div className="license">
-          <p>
-            Site design / logo © 2022 Stack Exchange Inc; <br />
-            user contributions licensed under CC BY-SA. rev 2022.9.1.42957
-          </p>
-        </div>
-      </SocialBox>
+      <FooterTextBox>
+        <LogoBox>
+          <FontAwesomeIcon icon={faStackOverflow} size="2x" />
+        </LogoBox>
+        <Col>
+          <ul>
+            <div className="title">STACK OVERFLOW</div>
+            <li>Questions</li>
+            <li>Help</li>
+          </ul>
+        </Col>
+        <Col>
+          <ul>
+            <div className="title">PRODUCTS</div>
+            <li>Teams</li>
+            <li>Advertising</li>
+            <li>Collectives</li>
+            <li>Talent</li>
+          </ul>
+        </Col>
+        <Col>
+          <ul>
+            <div className="title">COMPANY</div>
+            <li>About</li>
+            <li>Press</li>
+            <li>Work Here</li>
+            <li>Legal</li>
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>Contact Us</li>
+            <li>Cookie Settings</li>
+            <li>Cookie Policy</li>
+          </ul>
+        </Col>
+        <Col>
+          <ul>
+            <div className="title">STACK EXCHANGE NETWORK</div>
+            <li>Technology</li>
+            <li>Culture & recreation</li>
+            <li>Life & arts</li>
+            <li>Science</li>
+            <li>Professional</li>
+            <li>Business</li>
+            <li className="hr">안보이지</li>
+            <li>API</li>
+            <li>Data</li>
+          </ul>
+        </Col>
+        <SocialBox>
+          <ul>
+            <li>Blog</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Linkedin</li>
+            <li>Instagram</li>
+          </ul>
+          <div className="license">
+            <p>
+              Site design / logo © 2022 Stack Exchange Inc; <br />
+              user contributions licensed under CC BY-SA. rev 2022.9.1.42957
+            </p>
+          </div>
+        </SocialBox>
+      </FooterTextBox>
     </FooterLayout>
   );
 }
@@ -75,14 +77,26 @@ function Footer() {
 export default Footer;
 
 const FooterLayout = styled.div`
-  justify-content: center;
   background-color: #414141;
   width: 100%;
-  height: 30%;
+
+  box-sizing: border-box;
   display: flex;
-  padding: 10px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
 `;
 
+const FooterTextBox = styled.div`
+  top: 0;
+  bottom: 0;
+  width: 90%;
+  padding: 1rem;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+`;
 const LogoBox = styled.div`
   color: #bebebe;
 `;
@@ -118,9 +132,5 @@ const SocialBox = styled.div`
   }
 
   .license {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 `;
