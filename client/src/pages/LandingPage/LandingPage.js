881f1659c41ba5f6cc2368e3a5d6d24551b69dc0
songseul <song.codestates@gmail.com>
2022-09-06 11:47:32 +0900

    feat:메인(로그인안한)페이지 스타일 구현

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Questions from '../Questions/Questions';
-
+import Main from './Main';
 /**
  * TODO: 랜딩페이지는 로그인 여부를 상태로 체크하여 로그인 x 일 경우 일반 랜딩 페이지, 로그인 되었을 경우 Questions 컴포넌트가 보여지도록 추후 수정합니다.
  *
@@ -12,7 +12,9 @@ function LandingPage() {
       {localStorage.loginStatus === 'true' ? (
         <Questions />
       ) : (
-        <div>랜딩페이지</div>
+        <div>
+          <Main />
+        </div>
       )}
     </div>
   );
